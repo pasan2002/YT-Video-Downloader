c6eec2229200139c6c90c2807073ea0fc6a23168
Pasan Hewavitharana <115849043+pasan2002@users.noreply.github.com>
2024-04-04 18:51:28 +0530

    Update Downloader.jsx

--- ytdownloader/src/Downloader.jsx
+++ ytdownloader/src/Downloader.jsx
@@ -27,7 +27,7 @@ export default function Download() {
             const videoID = videoIDMatch[1]
 
             const response = await fetch(
-                `https://yt-video-downloader-gamma.vercel.app/api/v1/get-vid-info/${videoID}`
+                `https://youtube-video-downloader-extension-1.onrender.com/api/v1/get-vid-info/${videoID}`
             );
 
             if (!response.ok) {
@@ -36,7 +36,7 @@ export default function Download() {
 
             const data = await response.json()
             const durationResponse = await fetch(
-                `https://yt-video-downloader-gamma.vercel.app/api/v1/get-video-duration/${videoID}`
+                `https://youtube-video-downloader-extension-1.onrender.com/api/v1/get-video-duration/${videoID}`
             )
             if (!durationResponse.ok) {
                 throw new Error(`API request failed with status ${durationResponse.status}`)
@@ -62,7 +62,7 @@ export default function Download() {
             /youtu(?:\.be|be\.com)\/(?:[\w\-]+\?v=)?([^\?&\"\<>]+)/
         )
         const videoID = videoIDMatch[1]
-        const url = `https://yt-video-downloader-gamma.vercel.app/video-download?id=${videoID}&resolution=${resolution}`
+        const url = `https://youtube-video-downloader-extension-1.onrender.com/video-download?id=${videoID}&resolution=${resolution}`
         window.location.href = url
     }
 
