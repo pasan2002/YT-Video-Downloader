56238c4d1ce4b68ded93afb8f13f62b15595af7d
Pasan Hewavitharana <115849043+pasan2002@users.noreply.github.com>
2024-04-28 11:25:44 +0530

    Update App.js

--- ytdownloader/src/App.js
+++ ytdownloader/src/App.js
@@ -1,11 +1,16 @@
 import React from "react" 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Download from "./Downloader"
+import Insta from "./Insta";
 
 function App() {
   return (
-    <div className="App">
-      <Download/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Download/>}/>
+        <Route path="/insta" element={<Insta/>}/>
+      </Routes>
+    </Router>
   );
 }
 
