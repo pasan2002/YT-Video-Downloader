1a657f2bae227a577a2a158df4cc4ee722f737e4
Pasan Hewavitharana <115849043+pasan2002@users.noreply.github.com>
2024-05-02 08:54:52 +0530

    Update App.js

--- ytdownloader/src/App.js
+++ ytdownloader/src/App.js
@@ -1,7 +1,8 @@
 import React from "react" 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { MemoryRouter as Router, Routes, Route } from "react-router-dom";
 import Download from "./Downloader"
 import Insta from "./Insta";
+import Facebook from "./Facebook";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Download/>}/>
         <Route path="/insta" element={<Insta/>}/>
+        <Route path="/facebook" element={<Facebook/>}/>
       </Routes>
     </Router>
   );
